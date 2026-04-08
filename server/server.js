d00877d646eaba0408b8fab49ef4ca60e2a2f8e6
Isabelle <isabelle.dahlstromm@gmail.com>
2026-04-08 13:06:22 +0200

    feat:  setup express server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+//Load variables from .env file
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+
+// Connecting the database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch(err => console.error(err));
