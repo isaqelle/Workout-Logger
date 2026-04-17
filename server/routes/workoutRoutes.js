bfd89d545fd877c4919e8672869463a0322e9ada
Isabelle <isabelle.dahlstromm@gmail.com>
2026-04-17 15:08:16 +0200

    feat: add workouts-per-user statistics endpoint

--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Workout from "../models/Workout.js";
+import User from "../models/User.js"
+import Exercise from "../models/Exercise.js"
 
 const router = express.Router();
 
@@ -18,11 +20,45 @@ router.post("/", async (req, res) => {
 // GET all workouts
 router.get("/", async (req, res) => {
     try {
-        const workouts = await Workout.find();
+        //filtering
+        const { userId } = req.query;
+        let filter = {};
+        if (userId) {
+            filter.userId = userId;
+        }
+
+        const workouts = await Workout.find(filter)
+            .populate("userId") // populate user
+            .populate("exercises.exerciseId"); // populate nested exercise
         res.json(workouts);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
+
+
+// GET total workouts per user
+router.get("/stats/workout-counter", async (req, res) => {
+    try {
+        const workouts = await Workout.find().populate("userId", "name");
+        const counts = {};
+        
+        workouts.forEach(workout => {
+            const userName = workout.userId.name;
+            if (!counts[userName]) {
+                counts[userName] = 0;
+            }
+            counts[userName]++;
+        });
+
+        const result = Object.keys(counts).map(name => ({
+            user: name,
+            totalWorkouts: counts[name]
+        }));
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
 
 export default router;
