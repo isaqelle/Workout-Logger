46dbb936a4d3bec7857960247f79909b140497bb
Isabelle <isabelle.dahlstromm@gmail.com>
2026-04-26 15:26:01 +0200

    feat: loading and error states implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,27 @@ import WorkoutForm from "./components/WorkoutForm";
 import ExerciseForm from "./components/ExerciseForm";
 
 function App() {
-
-
   const [workouts, setWorkouts] = useState([]);
+
+  // for error and loading states
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/workouts")
       .then(res => res.json())
       .then(data => {
-
         setWorkouts(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError("Failed to load workouts");
+        setLoading(false);
       });
-      
   }, []);
+    
+
+
 
   //DELETE WORKOUT
   const deleteWorkout = async (id) => {
@@ -49,6 +57,8 @@ function App() {
     setWorkouts(workouts.map(w => w._id === id ? updated : w))
   }
 
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error}...</p>
 
   return (
     <div>
@@ -60,7 +70,8 @@ function App() {
       }/>
       <WorkoutList workouts={workouts} onDelete={deleteWorkout} onUpdate={updateWorkout} />
 
-      <ExerciseForm onExerciseAdded={()=> window.location.reload()}></ExerciseForm>
+      <ExerciseForm onExerciseAdded={() => window.location.reload()}></ExerciseForm>
+      
       
     </div>
   );
